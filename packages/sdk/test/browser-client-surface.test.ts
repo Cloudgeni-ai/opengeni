31ad12f6b93c4e4884c8aabb8a954e9d48b44295
Jørgen Sandhaug <jorgen.sandhaug@gmail.com>
2026-08-31 11:32:28 +0200

    test(sdk): update browser client usage contract

--- packages/sdk/test/browser-client-surface.test.ts
+++ packages/sdk/test/browser-client-surface.test.ts
@@ -11,7 +11,6 @@ const legacyBrowserUnusedMethods = [
   "addDocument",
   "advanceExternalBrowserAuthRun",
   "applyGoalRevision",
-  "cancelSession",
   "captureComputerTarget",
   "codexAccountUsage",
   "codexDisconnect",
